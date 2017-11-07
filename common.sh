7000f85c6b93f8fdce818cd09755040193f70f03
ivan100sic <ivan100sic@gmail.com>
2017-11-07 17:16:31 +0000

    Update common.sh

--- common.sh
+++ common.sh
@@ -14,6 +14,7 @@ done;
 gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ hsize 4
 gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ vsize 4
 wget https://i.ytimg.com/vi/sJQy7inDgUE/maxresdefault.jpg -O zid.jpg
+chmod 666 zid.jpg
 gsettings set org.gnome.desktop.background picture-uri 'file:///home/ubuntu/zid.jpg'
 # gsettings set org.gnome.desktop.interface clock-show-seconds true
 
