b6d9548befaefd0b0073658305c1aeb9a5a9e55f
ivan100sic <ivan100sic@gmail.com>
2017-11-07 11:20:17 +0000

    Update common.sh

--- common.sh
+++ common.sh
@@ -22,6 +22,7 @@ apt-get install apt-transport-https
 # Install sublime text 3
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+apt-get update
 sudo apt-get install sublime-text
 
 # Install various utilities
