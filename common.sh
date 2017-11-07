e2c44d9b318488cf91da25549e387d6252f87bbe
ivan100sic <ivan100sic@gmail.com>
2017-11-07 17:31:54 +0000

    Update common.sh

--- common.sh
+++ common.sh
@@ -1,6 +1,6 @@
-# Make sure we are root
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
+# Make sure we are NOT root
+if [ "$EUID" -e 0 ]
+  then echo "Please do not run as root, you may not see some changes"
   exit
 fi
 
@@ -14,24 +14,20 @@ done;
 gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ hsize 4
 gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ vsize 4
 wget https://i.ytimg.com/vi/sJQy7inDgUE/maxresdefault.jpg -O zid.jpg && \
-chmod 666 zid.jpg && \
-chown ubuntu zid.jpg && \
 gsettings set org.gnome.desktop.background picture-uri 'file:///home/ubuntu/zid.jpg'
 
-
 # Update apt
-apt-get update
-apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install apt-transport-https
 
 # Install sublime text 3
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-apt-get update
-apt-get install sublime-text
+sudo apt-get update
+sudo apt-get install sublime-text
 
 # Install various utilities
-apt-get install curl git
-
+sudo apt-get install curl git
 
 
 
