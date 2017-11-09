d65ab2c3f899ee2df206a1a548b67774de687a60
ivan100sic <ivan100sic@gmail.com>
2017-11-09 01:35:49 +0100

    Update common.sh

--- common.sh
+++ common.sh
@@ -17,17 +17,17 @@ wget https://i.ytimg.com/vi/sJQy7inDgUE/maxresdefault.jpg -O zid.jpg && \
 gsettings set org.gnome.desktop.background picture-uri 'file:///home/ubuntu/zid.jpg'
 
 # Update apt
-sudo apt-get update
-sudo apt-get install apt-transport-https
+sudo apt-get -y update
+sudo apt-get -y install apt-transport-https
 
 # Install sublime text 3
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update
-sudo apt-get install sublime-text
+sudo apt-get -y update
+sudo apt-get -y install sublime-text
 
 # Install various utilities
-sudo apt-get install curl git
+sudo apt-get -y install curl git
 
 
 
